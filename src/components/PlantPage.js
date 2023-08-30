8c9506b413fc8e014c10f06b7410f1019022c5de
Kim Love <kimberlymlove15@gmail.com>
2023-08-30 10:46:08 -0400

    First core deliverable. All plants render to page

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const url = "http://localhost:6001/plants"
+  const [ plants, setPlants ] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setPlants(data))
+  }, [])
+
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
