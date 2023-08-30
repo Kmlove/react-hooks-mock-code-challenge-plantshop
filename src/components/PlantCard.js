8e9972d4fc77f4c9bca9d1ff36502f0278eabe93
Kim Love <kimberlymlove15@gmail.com>
2023-08-30 10:53:37 -0400

    In/out of stock button click working

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({plant}) {
   const {image, name, price} = plant
+  const [ inStock, setInStock ] = useState("true")
+
+  function handleClick(){
+    setInStock(false)
+  }
+
+  const buttonText = inStock ? "In Stock" : "Out of Stock"
 
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
